efe62f80dca50a79e60ccd2e96a073ed997957bc
sharma-divyanshi <145781621+sharma-divyanshi@users.noreply.github.com>
2025-06-26 17:57:12 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Count from './count';
+import Like from './islike';
+import Func from './useEffect';
+import Object from './state_with_object';
+import Array from './assets/arraystate';
+
+function App() {
+  
+
+  return (
+    <>
+     <Array/>  
+    </>
+  )
+}
+
+export default App
